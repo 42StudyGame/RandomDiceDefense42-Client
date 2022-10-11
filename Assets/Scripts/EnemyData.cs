7707c8afe76a966ff1c67648878644555c73a021
yurudehd <gyyu@student.42seoul.kr>
2022-10-11 15:47:34 +0900

    적과 총알 충돌시 체력감소 및 죽음 구현

--- Assets/Scripts/EnemyData.cs
+++ Assets/Scripts/EnemyData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Scriptable/EnemyData", fileName = "Enemy Data")]
 public class EnemyData : ScriptableObject {
 	public string type = "Normal";
-	public int health = 100;
+	public float health = 100;
 	public int sp = 10;
 	public float speed = 1f;
 	public Sprite sprite;
