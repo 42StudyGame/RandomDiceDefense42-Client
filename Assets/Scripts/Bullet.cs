5c8cd5a9351b78d3c7072837b87e15288825cdee
noname <cloudeven@gmail.com>
2022-10-11 16:58:29 +0900

    refactor: bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,66 +1,77 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public partial class Bullet // IO
 {
-	
+	public void SetTarget(Enemy target) => _SetTarget(target);
+	public void SetDamage(float damage) => _SetDamage(damage);
+	public void Init(BulletPool bulletPool) => _Init(bulletPool);
 }
 
-public partial class Bullet // SerializeField
-{
-	
-}
-
-public partial class Bullet //IO
-{
-	
-}
+// public partial class Bullet // SerializeField
+// {
+// }
 
 public partial class Bullet : MonoBehaviour
 {
 	private Enemy _target;
-	private const float MovementSpeed = 10f;
 	private BulletPool _pool;
 	private Collider2D _targetCollider;
-	private float _damage;
-
-	public void SetTarget(Enemy target)
-	{
-		_target = target;
-		_targetCollider = _target.GetComponent<Collider2D>();
-		// getComponent 안쓸수 있으면 좋음. 비싸고, 빈도도 잦음
-	}
-
-	public void SetDamage(float damage) {
-		_damage = damage;
-	}
-	public void Init(BulletPool bulletPool) {
-		_pool = bulletPool;
-	}
 	
-	private void Update() {
+	private void Update() 
+	{
 		if (_target)
+		{
 			Move();
+		}
 		else
+		{
 			_pool.ReturnObject(this);
+		}
 	}
 
-	private void OnTriggerEnter2D(Collider2D col) {
+	private void OnTriggerEnter2D(Collider2D col) 
+	{
 		if (col == _targetCollider)
 		{
 			_pool.ReturnObject(this);
 			_target.OnDamage(_damage);
 		}
 	}
+}
 
-	private void Move() {
+public partial class Bullet // body
+{
+	private const float MovementSpeed = 10f;
+	private float _damage;
+
+	private void _SetTarget(Enemy target)
+	{
+		_target = target;
+		_targetCollider = _target.GetComponent<Collider2D>();
+	}
+
+	private void _SetDamage(float damage) 
+	{
+		_damage = damage;
+	}
+
+	private void _Init(BulletPool bulletPool)
+	{
+		_pool = bulletPool;
+	}
+
+	private void Move() 
+	{
 		if (!_target || !_target.gameObject.activeInHierarchy)
+		{
 			_pool.ReturnObject(this);
+		}
+
 		transform.position = Vector3.MoveTowards
-		(transform.position, 
+		(transform.position,
 			_target.transform.position, 
 			MovementSpeed * Time.deltaTime);
 	}
-	
+
 	// z fighting
 }
