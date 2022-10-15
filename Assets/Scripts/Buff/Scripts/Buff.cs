48fcf86c36e93670652be36ec67cf41ed0ee1cec
cloudeven <cloudeven@gmail.com>
2022-10-16 06:34:56 +0900

    #42 exclude buff.cs

---
+++ Assets/Scripts/Buff/Scripts/Buff.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class Buff // IO
+{
+}
+
+public partial class Buff // SerializeField
+{
+}
+
+public partial class Buff : MonoBehaviour
+{
+}
+
+public partial class Buff // body
+{
+}
