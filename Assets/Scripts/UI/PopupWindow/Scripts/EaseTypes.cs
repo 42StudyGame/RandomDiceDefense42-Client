9712975911f548f9b4ecde56c7f45139728fec62
noname <cloudeven@gmail.com>
2022-10-26 02:47:59 +0900

    .

--- Assets/Scripts/UI/PopupWindow/Scripts/EaseTypes.cs
+++ Assets/Scripts/UI/PopupWindow/Scripts/EaseTypes.cs
@@ -12,34 +12,13 @@ public partial class Ease // IO
     public void SetReverse() => _setReverse();
 }
 
-public partial class Ease // body
+public partial class Ease // Common
 {
     private float _duration;
     private float _beginTime;
     private EaseType _easeType;
     private UnityAction _onComplete;
-
-    private Vector2 _vectorNormalizedOffset;
-    private Vector2 _vectorBegin;
-    private Vector2 _vectorEnd;
-
-    public Ease(Vector2 begin, Vector2 end, float duration, EaseType easeType = EaseType.Linear, UnityAction onComplete = null)
-    {
-        _setData(begin, end, duration, easeType, onComplete);
-    }
     
-    private void _setData(Vector2 begin, Vector2 end, float duration, EaseType easeType, UnityAction onComplete)
-    {
-        _duration = duration;
-        _beginTime = Time.time;
-        _easeType = easeType;
-        _onComplete = onComplete;
-        
-        _vectorNormalizedOffset = (end - begin) / duration;
-        _vectorBegin = begin;
-        _vectorEnd = end;
-    }
-
     private Vector2 _nowValue()
     {
         return _easeType switch
@@ -49,16 +28,41 @@ public partial class Ease // body
         };
     }
 
+    private void _setCommonData(float duration, EaseType easeType, UnityAction onComplete)
+    {
+        _duration = duration;
+        _beginTime = Time.time;
+        _easeType = easeType;
+        _onComplete = onComplete;
+    }
+}
+
+public partial class Ease // EaseType.Linear
+{
+    private Vector2 _vectorNormalizedOffset;
+    private Vector2 _vectorBegin;
+    private Vector2 _vectorEnd;
+
+    public Ease(Vector2 begin, Vector2 end, float duration, EaseType easeType = EaseType.Linear, UnityAction onComplete = null)
+    {
+        _setCommonData(duration, easeType, onComplete);
+        _setVectorData(begin, end, duration);
+    }
+    
+    private void _setVectorData(Vector2 begin, Vector2 end, float duration)
+    {
+        _vectorNormalizedOffset = (end - begin) / duration;
+        _vectorBegin = begin;
+        _vectorEnd = end;
+    }
+
     private void _setReverse()
     {
         _beginTime = Time.time;
         _vectorNormalizedOffset *= -1;
         (_vectorBegin, _vectorEnd) = (_vectorEnd, _vectorBegin);
     }
-}
-
-public partial class Ease // variation
-{
+    
     private Vector2 _linearProgress()
     {
         float timeSpan = Mathf.Min(Time.time - _beginTime, _duration);
@@ -70,4 +74,8 @@ public partial class Ease // variation
 
         return _vectorBegin + timeSpan * _vectorNormalizedOffset;
     }
+}
+
+public partial class Ease // EaseType.something
+{
 }
