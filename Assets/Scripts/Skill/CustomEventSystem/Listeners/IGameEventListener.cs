9f2a1a6dd8833065e2b46ec4e5c6630a8ea8f5e9
seonhjeo <hoya011196@gmail.com>
2022-11-09 16:24:19 +0900

    feat: implement basic skill system

--- Assets/Scripts/Skill/CustomEventSystem/Listeners/IGameEventListener.cs
+++ Assets/Scripts/Skill/CustomEventSystem/Listeners/IGameEventListener.cs
@@ -2,6 +2,4 @@
 public interface IGameEventListener<T>
 {
     void OnEventRaised(T item);
-
-
 }
---
+++ Assets/Scripts/Skills/SkillDataSO.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/SkillData/OneOff", fileName = "One-off Skill Data")]
+public class OneOffSkillData : ScriptableObject
+{
+    public int id;
+
+    public void effect()
+    {}
+}
